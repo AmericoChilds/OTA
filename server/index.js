dfb18e97703c3e92c80931824c23a2d6560960e8
Americo Childs <americochilds@gmail.com>
2021-09-24 15:54:49 -0500

    hotfix

--- server/index.js
+++ server/index.js
@@ -21,7 +21,9 @@ app.use('/spaces', spacesRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const url = process.env.CONNECTION_URL;
+console.log(url);
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
