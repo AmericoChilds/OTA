9bb64e604b118bfafba25e091be0a119728ec179
Americo Childs <americochilds@gmail.com>
2021-09-24 15:36:20 -0500

    fixed env files

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 import spacesRoutes from './routes/spaces.js';
 
-import CONNECTION_URL from './protected.js';
+//import CONNECTION_URL from './protected.js';
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use('/spaces', spacesRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
